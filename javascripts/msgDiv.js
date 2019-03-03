ee8a286e7868313872b0e438e61abab5c4be92b6
Star of Aquarius <crystalfranks18@hotmail.com>
2019-03-02 23:24:11 -0500

    Uppy!

---
+++ javascripts/msgDiv.js
@@ -0,0 +1,66 @@
+	var msgArr = new Array();
+msgArr[0] = "Even the stars will fly out, should the world can be doomed?";
+msgArr[1] = "Heavens are always upon us!";
+msgArr[3] = "Only 60% of people are aware of the autists' habits of running around in circles.";
+msgArr[4] = "This website is powered by a <a href='https://www.walmart.com/'>Walmart store</a>! Just kidding.";
+msgArr[5] = "I only wrote a story about a brony who plays Super Mario Maker.";
+msgArr[6] = "Internet is full of surprises. Sometimes it isn't.";
+msgArr[7] = "One psychic power is enough to make anything happen. Plain and simple!";
+msgArr[8] = "The loud wail causes the family to leave the carnival to home without warning.";
+msgArr[9] = "Whoever saw the image of the thing they hate can be visited to the hospital right away.";
+msgArr[10] = "Hi! I'm Daisy!";
+msgArr[11] = "Peppers? Um, they don't do it on my spicy pepperoni burgers, right?";
+msgArr[12] = "I once checked someone's inflation picture with the minors any website.";
+msgArr[13] = "Like my status quo?";
+msgArr[14] = "<b class='rainbow'>WHOAH! Slow down, kiddo!</b>";
+msgArr[15] = "Does this remind you of kawaii stuff so much?";
+msgArr[16] = "Patrick, the mayonnaise here is not an instrument!";
+msgArr[17] = "I think I changed my mind.";
+msgArr[18] = "What did you mean it's for most ages?";
+msgArr[19] = "Now put that thing back where it came from or so help me!";
+msgArr[20] = "Hey! A 20th message!";
+msgArr[21] = "I'll eat a hot dog.<br>Actually, I put wax on your hot dog.<br>WHAT!?";
+msgArr[22] = "No creatures were harmed in the making of this website.";
+msgArr[23] = "Yeah, yeah! Friendship and flowers and ponies! Bleh! I'm so totally over the cute pony thing.";
+msgArr[24] = "Good things always happened!";
+msgArr[25] = "Humanuh humanuh humanuh humanuh...<br>Website for humanuh?";
+msgArr[26] = "<img src='https://cdn.bulbagarden.net/upload/9/93/Bag_Pok%C3%A9_Ball_Sprite.png'><br>Oh wow! A Pok&eacute Ball! How did it get here?";
+msgArr[27] = "I've gotta act fast!";
+msgArr[28] = "OOOOOOOOOOOOOOOOOH NOOOOOOOOOOOOOOOOOOOOO!!!";
+msgArr[29] = "Holy shrimp, Batman! We're trapped!";
+msgArr[30] = "I went to the Miracle Star homepage and got a popup, asking me if I like waffles or not.";
+msgArr[31] = "Miracle Star is once called Super Jump Punch, but it undergoes many transformations over years.";
+msgArr[32] = "Thumbs up if you see a logo flashing on New Years Day!";
+msgArr[33] = "PARMESAN CHEEEEEEEEEEEEEEEEEEEEESE!!!! With cheddar.";
+msgArr[34] = "Rumours say some guy once visited the Beanbean Kingdom and receive a message that the villainess wanted his address and phone number.";
+msgArr[35] = "Great Hammer Barrage, everyone! I got shocked by the one of the shock images!";
+msgArr[36] = "NOTE: If you see this message, please copy it into your DeviantART signature. Thank you!";
+msgArr[37] = "Some say mushrooms don't grow on trees.";
+msgArr[38] = "This world is built by someone who is always innocent. Just hope to ignore this.";
+msgArr[39] = "Oh yeah? Try and figure your way out of this!";
+msgArr[40] = "Don't ask me where I got this message from!";
+msgArr[41] = "I was playing Adventures of Lolo fan game and find out that there is an evidence of a cartoon man from America getting angry at me. If I keep playing this game, he'll brutally hurt me.";
+msgArr[42] = "Who makes a chocolate pudding at 4:00 AM?<br>Stuart Pickles!";
+msgArr[43] = "Holy Fudge! Why is this site's already getting old!?";
+msgArr[44] = "PASTAAAAAAAAAAAAAAA!!!!! Just kidding!";
+msgArr[45] = "I'm going to send a thank-you gift to Squidward... TORTELLINI!?";
+msgArr[45] = "This world isn't garbage. Trust me.";
+msgArr[46] = "Don't make me grow cat whiskers on my face!";
+msgArr[47] = "Erica: Maxie! Why the heck are you making an orange Jell-O at 3 in the morning!?<br>Max: Because I cannot control of my life!";
+msgArr[48] = "Stare at the spiral for 10 minutes, and then look at yourself!";
+msgArr[49] = "Who says I'm starting this meme!?";
+msgArr[50] = "This 50th quote is MINE!";
+msgArr[51] = "Down the hatch, sir!";
+msgArr[52] = "It's Waka-Laka time!";
+msgArr[53] = "SEGA is what Nintendon't!";
+msgArr[54] = "Shut up, net-face!<br>Hey, you're a net-face!";
+msgArr[55] = "How cheesy that dog's whiskers are!";
+msgArr[56] = "This site's power is over 9000!";
+msgArr[57] = "Get rid of wasps! Or bees.";
+msgArr[58] = "Release the hounds!;"
+msgArr[59] = "Hungry? Get Gumby's Pizza!";
+msgArr[60] = "A 60-Hit Knockdown!"
+
+	window.onload = function () {
+		document.getElementById("msgDiv").innerHTML = msgArr[Math.floor(Math.random()*msgArr.length)];
+	}
